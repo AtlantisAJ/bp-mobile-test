9d8f919596aa953e7e5b332ecd91f053136faa23
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-04-22 10:07:14 +0300

    fix continue bag

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { adjustFontSize } from './utils/adjustFontSize.js';
 import { updateTranslations } from './translations/updateTranslations.js';
 import { initializeModal } from './components/modal.js';
 import { initializePricing } from './components/pricing.js';
-import { initializeContinueButton } from './components/continueButton.js';
 import de from './locales/de.json';
 import en from './locales/en.json';
 import es from './locales/es.json';
@@ -14,19 +13,18 @@ import pt from './locales/pt.json';
 
 const translations = { de, en, es, fr, ja, pt };
 const prices = {
-  yearly: '$39.99',
-  weekly: '$6.99',
-  yearly_per_week: '$0.48',
+    yearly: '$39.99',
+    weekly: '$6.99',
+    yearly_per_week: '$0.48',
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const lang = getLanguage();
-  const selectedTranslations = translations[lang] || translations.en;
+    const lang = getLanguage();
+    const selectedTranslations = translations[lang] || translations.en;
 
-  document.documentElement.lang = lang;
-  updateTranslations(selectedTranslations, prices);
-  adjustFontSize();
-  initializeModal();
-  initializePricing(prices);
-  initializeContinueButton();
+    document.documentElement.lang = lang;
+    updateTranslations(selectedTranslations, prices);
+    adjustFontSize();
+    initializeModal();
+    initializePricing();
 });
